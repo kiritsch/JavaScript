f8e4b59c9ea4f54f205f8f56fc8de2c118570850
kiritsch <91454517+kiritsch@users.noreply.github.com>
2021-11-14 17:34:06 +0300

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let count = 85;
+
+if (number > 89){
+    console.log("отлично");
+} else if (59 < number < 90){
+    console.log("хорошо");
+} else if (39 < number < 58){
+    console.log("удовлетворительно");
+} else if (0 < number < 38){
+    console.log("попробуйте еще раз");
+}
