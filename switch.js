d0c63e5de923bb3f6aa2bb272f334b462ebb023f
kiritsch <91454517+kiritsch@users.noreply.github.com>
2021-11-14 20:15:56 +0300

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let a = 8;
+let b = 4;
+
+switch (num) {
+    case 12:
+        console.log(a + b);
+        break;
+    case 4:
+        console.log(a - b);
+        break;
+    case 32:    
+        console.log(a * b);
+        break;
+    case 2:
+        console.log(a / b);
+        break;
+}
